e26aa5774ace1fa1d4152b7c9c9b2e08cfc017db
Veljko Tekelerović <veljko.tekelerovic@gmail.com>
2020-01-17 17:29:42 +0100

    Xcode project generation

---
+++ Sources/YOLOCoreML/YOLOCoreML.swift
@@ -0,0 +1,3 @@
+struct YOLOCoreML {
+    var text = "Hello, World!"
+}
---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,7 @@
+import XCTest
+
+import YOLOCoreMLTests
+
+var tests = [XCTestCaseEntry]()
+tests += YOLOCoreMLTests.allTests()
+XCTMain(tests)
